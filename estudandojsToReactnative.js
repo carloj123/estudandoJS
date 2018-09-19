00ca092b1503dccc847a92ad508f77699d4d7018
carloj123 <carlo.laitano@acad.pucrs.br>
2018-09-18 21:02:35 -0300

    poo

--- estudandojsToReactnative.js
+++ estudandojsToReactnative.js
@@ -9,13 +9,15 @@ class Pessoa{
 	 caminhar(){
 	 	document.write("caminhando");
 	 }
-	 getInformaoces(){
+	 getInformacoes(){
 	 	document.write(" nome: "+ this.nome+" idade: "+this.idade+" tem filhos? "+ this.temfilhos);
 	 }
 }
 
 var pessoa = new Pessoa("jonas",20,false);
-
+alert(pessoa);
 //pessoa.caminhar();
 document.write("Informacoes sobre essa pessoa");
+console.log(pessoa.getInformacoes());
 pessoa.getInformacoes();
+alert(pessoa.getInformacoes());
